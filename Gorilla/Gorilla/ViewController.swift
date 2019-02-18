88910e5bc6cfa65ec84025b44dacc3a05afcbd0c
Andrew Galushka <andrewgalushka@yahoo.com>
2019-02-18 17:46:02 +0200

    initial commit

---
+++ Gorilla/Gorilla/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Gorilla
+//
+//  Created by Galushka on 2019-02-18.
+//  Copyright © 2019 Galushka.com. All rights reserved.
+//
+
+import UIKit
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+
+}
