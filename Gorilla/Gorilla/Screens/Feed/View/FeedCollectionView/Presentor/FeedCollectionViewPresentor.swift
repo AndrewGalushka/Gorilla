db58713d96c69a339efaa0a00a891825620a3317
Andrew Galushka <andrewgalushka@yahoo.com>
2019-03-27 18:08:31 +0200

    Added Presenter class for FeedCollectionView

---
+++ Gorilla/Gorilla/Screens/Feed/View/FeedCollectionView/Presentor/FeedCollectionViewPresentor.swift
@@ -0,0 +1,12 @@
+//
+//  FeedCollectionViewPresentor.swift
+//  Gorilla
+//
+//  Created by Galushka on 3/27/19.
+//  Copyright © 2019 Galushka.com. All rights reserved.
+//
+
+import UIKit
+
+class FeedCollectionViewPresentor: FeedCollectionViewPresentorInput {
+}
---
+++ Gorilla/Gorilla/Screens/Feed/View/FeedCollectionView/Presentor/FeedCollectionViewPresentorInput.swift
@@ -0,0 +1,12 @@
+//
+//  FeedCollectionViewPresentorType.swift
+//  Gorilla
+//
+//  Created by Galushka on 3/27/19.
+//  Copyright © 2019 Galushka.com. All rights reserved.
+//
+
+import UIKit
+
+protocol FeedCollectionViewPresentorInput: AnyObject {
+}
