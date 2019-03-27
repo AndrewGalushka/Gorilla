39c65b90c3e9a48c302bb31c60617c136f2cf7dc
Andrew Galushka <andrewgalushka@yahoo.com>
2019-03-27 18:18:26 +0200

    Added method to make DataSource

--- Gorilla/Gorilla/Screens/Feed/View/FeedCollectionView/Presentor/FeedCollectionViewPresentor.swift
+++ Gorilla/Gorilla/Screens/Feed/View/FeedCollectionView/Presentor/FeedCollectionViewPresentor.swift
@@ -9,4 +9,28 @@
 import UIKit
 
 class FeedCollectionViewPresentor: FeedCollectionViewPresentorInput {
+    
+    private typealias CellViewModel = FeedCollectionImageViewCellViewModel
+    
+    // MARK: Methods(Private)
+    
+    private func makeDataSource() -> CollectionViewDataSource<CellViewModel> {
+        let configurator = self.makeConfigurator()
+        let dataSource = CollectionViewDataSource<CellViewModel>(dataSource: DataSource<CellViewModel>(),
+                                                                 configurator: configurator)
+        
+        return dataSource
+    }
+    
+    private func makeConfigurator() -> AnyCollectionViewCellConfigurator<CellViewModel> {
+        
+        let imageViewCellConfigurator = CollectionViewCellConfigurator<CellViewModel, FeedCollectionImageViewCell>.init { (cell, item, collectionView, indexPath) -> FeedCollectionImageViewCell in
+            cell.configure(viewModel: item)
+            return cell
+        }
+        
+        let anyConfigurator = AnyCollectionViewCellConfigurator<CellViewModel>(imageViewCellConfigurator)
+        
+        return anyConfigurator
+    }
 }
